0c30c9f5bf8058058606e26d2f54069f620bc597
sasquatch989 <solomon.j.sands@gmail.com>
2020-03-27 18:51:29 -0400

    Updated lab-routers inventory

--- inventory/lab-routers.sh
+++ inventory/lab-routers.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
 
-ROUTERS=$(ssh admin@172.20.20.1 "sh ip dhcp bind  | inc 01.*\.08" | cut -d' ' -f 1)
-printf "[lab-routers]\n%s" "$ROUTERS" > lab-routers.ini
 
-git add lab-routers.ini
-git commit -m "Updated lab-routers inventory"
-git push -f origin master
+
+
+get_routers() {
+  ROUTERS=$(ssh admin@172.20.20.1 "sh ip dhcp bind  | inc 01.*\.08" | cut -d' ' -f 1)
+  printf "[lab-routers]\n%s" "$ROUTERS" > lab-routers.ini
+}
+  
+prep_git() {
+  if git status | grep lab-routers; then
+	git add lab-routers.*
+  fi
+}
+
+ship_it() { 
+  git commit -m "Updated lab-routers inventory"
+  git push -f origin master
+}
+
+main() {
+  get_routers
+  prep_git
+  ship_it
+}
+
+
+main

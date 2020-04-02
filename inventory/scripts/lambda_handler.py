6d7e9cca8ba3fc18783e49e95450536fd73d2f1b
LAPTOP-8VSA4KB6\Solomon Sands <solomon.sands@cloudistics.com>
2020-04-01 23:53:57 -0400

    fixing...

--- inventory/scripts/lambda_handler.py
+++ inventory/scripts/lambda_handler.py
@@ -1,14 +1,14 @@
 import boto3
 from time import ctime
 
-
+url = ''
 def lambda_handler(event, context):
     d = boto3.resource('dynamodb')
     dT = d.Table('github_webhook_sqs')
     dT.put_item(Item={'event_date': ctime(), 'data': 'event received'})
 
     s = boto3.resource('sqs')
-    q = s.Queue('https://sqs.us-east-1.amazonaws.com/477286093069/github_webhook_sqs.fifo')
+    q = s.Queue(url)
     q.send_message(MessageBody=str(event), MessageGroupId="github_webhook_sqs")
 
     return {

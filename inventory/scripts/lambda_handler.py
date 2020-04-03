290f7ec72c730a2fff8f4c671c68eb6f14e786dd
LAPTOP-8VSA4KB6\Solomon Sands <solomon.sands@cloudistics.com>
2020-04-02 22:41:31 -0400

    fixing...

--- inventory/scripts/lambda_handler.py
+++ inventory/scripts/lambda_handler.py
@@ -1,17 +1,18 @@
+import json
 import boto3
+import os
 from time import ctime
-
-url = ''
 
 
 def lambda_handler(event, context):
     d = boto3.resource('dynamodb')
-    dT = d.Table('github_webhook_sqs')
-    dT.put_item(Item={'event_date': ctime(), 'data': 'event received'})
+    dt = d.Table('github_webhook_sqs')
+    dt.put_item(Item={'event_date': ctime(), 'data': 'Github push received'})
 
+    # Using env variable for sqs_url
     s = boto3.resource('sqs')
-    q = s.Queue(url)
-    q.send_message(MessageBody='Push Received', MessageGroupId="github_webhook_sqs")
+    sq = s.Queue(os.environ['sqs_url'])
+    sq.send_message(MessageBody='Github push received')
 
     return {
         'statusCode': 200,

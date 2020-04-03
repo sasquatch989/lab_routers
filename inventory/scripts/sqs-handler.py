e38a87b5b229cd9ff4d40a49d7dd11f402e2e8cc
LAPTOP-8VSA4KB6\Solomon Sands <solomon.sands@cloudistics.com>
2020-04-02 21:58:33 -0400

    fixing...

--- inventory/scripts/sqs-handler.py
+++ inventory/scripts/sqs-handler.py
@@ -1,37 +1,32 @@
 import boto3
+from time import sleep
 
 # Create SQS client
-queue_url = 'https://sqs.us-east-1.amazonaws.com/477286093069/github_webhook_sqs.fifo'
+queue_url = 'https://sqs.us-east-1.amazonaws.com/477286093069/github_webhook_sqs'
 sqs = boto3.resource('sqs', region_name='us-east-1')
 
 q = sqs.Queue(queue_url)
 
-# Receive message from SQS queue
-response = q.receive_messages(
+while True:
+    response = q.receive_messages(
+        AttributeNames=['SentTimestamp'],
+        MaxNumberOfMessages=10,
+        VisibilityTimeout=10,
+        WaitTimeSeconds=20
+    )
 
-    AttributeNames=[
-        'All'
-    ],
-    MaxNumberOfMessages=1,
-    MessageAttributeNames=[
-        'All'
-    ],
-    VisibilityTimeout=30,
-    WaitTimeSeconds=20
-)
-print(response)
-try:
-    message = response[0].body
-    receipt_handle = response[0].receipt_handle
-except IndexError:
-    exit()
-# Delete received message from queue
-q.delete_messages(
-    Entries=[
-        {
-            'Id': 'IdString',
-            'ReceiptHandle': receipt_handle
-        }
-    ]
-)
-print('Received and deleted message: %s' % message)
+    try:
+        message = response[0].body
+        receipt_handle = response[0].receipt_handle
+        #sleep(2)
+        q.delete_messages(
+            Entries=[
+                {
+                    'Id': response[0].attributes['SentTimestamp'],
+                    'ReceiptHandle': receipt_handle
+                }
+            ]
+        )
+        print('Message received and processed')
+    except IndexError:
+        print('Index is empty...')

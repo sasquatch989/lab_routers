fbc8e684d1bd7763256b7a6ddc648d2f5f70fc42
LAPTOP-8VSA4KB6\Solomon Sands <solomon.sands@cloudistics.com>
2020-04-05 17:30:12 -0400

    fixing...

--- handlers/tower/awxApi.py
+++ handlers/tower/awxApi.py
@@ -1,6 +1,32 @@
-import requests
+import json
 import subprocess
+import os
+import logging
+# # Make dynamic for project seperation
+
+def get_token():
+    if not os.getenv('TOWER_TOKEN'):
+        login = subprocess.run(['awx', 'login'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    try:
+        if json.loads(login.stdout)['token']:
+            os.environ['TOWER_TOKEN'] = json.loads(login.stdout['token'])
+    except json.decoder.JSONDecodeError:
+        print(login.stdout.decode())
+        exit()
 
 
+def api_call():
+    project_id = json.loads(
+        subprocess.run(
+            ['awx', 'projects', 'list', '--name', 'lab-routers'],
+            stdout=subprocess.PIPE
+        ).stdout
+    )['results'][0]['id']
+
+    return subprocess.run(['awx', 'projects', 'update', project_id])
 
 
+if __name__ == '__main__':
+    get_token()
+    api_call()
